beaa9b408b2610e6ee8713eac49a763d1fb7d14f
zhaoy <yan.zhao@idiaoyan.com>
2019-10-28 19:52:34 +0800

    add practise 1_1

---
+++ chapter1/1_1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", " "
+
+	for _, arg := range os.Args[1:] {
+		fmt.Println(arg)
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	fmt.Println(os.Args[0])
+}
