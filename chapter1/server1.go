1158f28c1296e70c8daef8db077b54c294361769
zhaoy <yan.zhao@idiaoyan.com>
2019-10-29 11:24:11 +0800

    add server1/count

--- chapter1/server1.go
+++ chapter1/server1.go
@@ -4,13 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var count int
+var mu sync.Mutex
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	fmt.Fprintf(w, "URL.path = %q, count is %d", r.URL.Path, count)
+	mu.Unlock()
 }
