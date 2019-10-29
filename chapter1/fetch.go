36032ddc97f02b9edc5ef34fe0f1fddd5a6f2134
zhaoy <yan.zhao@idiaoyan.com>
2019-10-29 15:05:55 +0800

    fix fetch

--- chapter1/fetch.go
+++ chapter1/fetch.go
@@ -2,7 +2,6 @@ package main
 
 //e1.5
 import (
-	"bufio"
 	//"bufio"
 	"fmt"
 	"io"
@@ -17,18 +16,10 @@ func main() {
 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 		os.Exit(1)
 	}
-	b, err := io.Copy(os.Stdout, resp.Body)
+	io.Copy(os.Stdout, resp.Body)
 	resp.Body.Close()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-		os.Exit(1)
-	}
-	input := bufio.NewScanner(os.Stdout)
-	for input.Scan() {
-		fmt.Println("---------------------")
-		fmt.Println("size: %d", b)
-
-		fmt.Printf("%s", input.Text())
-	}
-
+	//if err != nil {
+	//	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+	//	os.Exit(1)
+	//}
 }
