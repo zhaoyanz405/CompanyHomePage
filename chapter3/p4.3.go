a9dc3111f21c6770f59117fe199c40f8db0aaca2
zhaoy <yan.zhao@idiaoyan.com>
2019-10-30 19:54:23 +0800

    fix p4.3.go

--- chapter3/p4.3.go
+++ chapter3/p4.3.go
@@ -12,14 +12,18 @@ func reverse1(s []string) {
 
 func reverse2(s *[3]string) {
 	for i, j := 0, len(*s)-1; i < j; i, j = i+1, j-1 {
-		(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
+		s[i], s[j] = s[j], s[i]
 	}
 }
 
 func main() {
 	a := new([3]string)
 	*a = [3]string{"a", "b", "c"}
-
 	reverse2(a)
 	fmt.Println(*a)
+
+	b := [3]string{"a", "b", "c"}
+	reverse2(&b)
+	fmt.Println(b)
+
 }
